10859db33567f4239a208ad027cae05b09f3a461
Jesper Friis <jesper.friis@sintef.no>
2022-09-28 20:24:32 +0200

    Cleaned up the ontology.

--- astm.py
+++ astm.py
@@ -8,10 +8,55 @@ from ontopy import World
 import owlready2
 
 
-#thisdir = os.path.abspath(os.path.dirname(__file__))
-thisdir = Path(__file__).absolute().parent
+# Version of the ISO/ASTM standard (not direct accessable from html)
+version = "v1"
+
+# Base iri prepended to all concepts in the generated ontology
+astm_iri = "http://iso.org/astm#"
+
+# Header numbers, mapped to (header_name, header_documentation) tuples
+headers = {
+    "3.1": ("General terms",
+            "General terms related to additive manufacturing."),
+    "3.2": ("Process categories",
+            "Categorisation according to AM processing technique."),
+    "3.3": ("Processing: general processing",
+            "General terms related to AM processing."),
+    "3.4": ("Processing: data",
+            "Software-related terms."),
+    "3.5": ("Processing: positioning coordination and orientation",
+            "Terms related to AM positioning, coordination and "
+            "orientation."),
+    "3.6": ("Processing: material",
+            "Material-related terms for AM processing."),
+    "3.7": ("Processing: material extrusion",
+            "Material-related terms for AM extrusion"),
+    "3.8": ("Processing: powder bed fusion",
+            "Terms related to powder bed fusion."),
+    "3.9": ("Parts: general parts",
+            "AM products categorised according to structure."),
+    "3.10": ("Parts: applocations",
+            "Categorisation according to application within AM."),
+    "3.11": ("Parts: properties",
+            "Part-related properties."),
+    "3.12": ("Parts: evaluation",
+            "Terms related to evaluation of AM products."),
+}
+
+# Description of top-level headers (that cannot be derived from `headers`)
+topheaders = {
+    "Processing": "Terms related to AM processing.",
+    "Parts": "The class of all terms  within additive manufaturing "
+           "that are related to parts.",
+}
+
+# Additional rdfs:subClassOf relations
+subclassOf = {
+    "3DPrinting": ["AdditiveManufacturing"],
+}
 
 
+# -- Start of main script --
 def en(s):
     """Returns `s` as an English location string."""
     return owlready2.locstr(s, lang="en")
@@ -20,6 +65,8 @@ def en(s):
 def fixtitle(title):
     """Fix ASTM title."""
     if title.endswith(",noun"):
+        title = title[:-5]
+    if title.endswith(",verb"):
         title = title[:-5]
     if title.endswith(",adjective"):
         title = title[:-10]
@@ -41,9 +88,7 @@ def fixdef(definition):
     return definition if definition.endswith(".") else f"{definition}."
 
 
-version = "v1"
-astm_iri = "http://iso.org/astm#"
-
+thisdir = Path(__file__).absolute().parent
 world = World()
 
 
@@ -51,10 +96,16 @@ world = World()
 skos = world.get_ontology("http://www.w3.org/2004/02/skos/core#")
 with skos:
     class prefLabel(owlready2.rdfs.label):
-        pass
+        """The preferred label for a concept."""
 
     class altLabel(owlready2.rdfs.label):
-        pass
+        """Alternative label for a concept."""
+
+    class hiddenLabel(owlready2.rdfs.label):
+        """A lexical label for a resource that should be hidden when
+        generating visual displays of the resource, but should still
+        be accessible to free text search operations."""
+        comment = ["May be used for deprecated or misspelled labels."]
 
 
 # Selected annotations from Dublin Core terms
@@ -80,56 +131,46 @@ with dcterms:
 astm = world.get_ontology(astm_iri)
 astm.base_iri = astm_iri
 
-headers = {
-    "3.1": "General terms",
-    "3.2": "Process categories",
-    "3.3": "Processing: general processing",
-    "3.4": "Processing: data",
-    "3.5": "Processing: positioning coordination and orientation",
-    "3.6": "Processing: material",
-    "3.7": "Processing: material extrusion",
-    "3.8": "Processing: powder bed fusion",
-    "3.9": "Parts: general parts",
-    "3.10": "Parts: applocations",
-    "3.11": "Parts: properties",
-    "3.12": "Parts: evaluation",
-}
 
 with astm:
     class astmNo(owlready2.AnnotationProperty):
-        prefLabel = ["astmNo"]
-        comment = ["ASTM number."]
+        """ASTM number."""
 
     class astmId(owlready2.AnnotationProperty):
-        prefLabel = ["astmId"]
-        comment = ["ASTM id."]
+        """Full ISO/ASTM identifier."""
 
-    class astmDef(owlready2.AnnotationProperty):
-        prefLabel = ["astmDef"]
-        comment = ["ASTM definition."]
+    class astmDef(owlready2.rdfs.comment):
+        """ASTM definition."""
 
     class ASTMType(owlready2.Thing):
-        prefLabel = ["ASTMType"]
-        comment = ["Type categorisation."]
+        """Categorisation according to grammatical category in linguistics."""
 
     class Noun(ASTMType):
-        prefLabel = ["Noun"]
+        """Terms that are nouns, i.e. that are names of something."""
+
+    class Verb(ASTMType):
+        """Terms that are verbs."""
 
     class Adjective(ASTMType):
-        prefLabel = ["Adjective"]
+        """Terms that describe or limit/restrict the meaning of a noun.
+        Typically a condition."""
 
     class Participle(ASTMType):
-        prefLabel = ["Participle"]
+        """A verbal that expresses a state of an entity."""
 
-    for headerno, header in headers.items():
+
+    # Add classes from headers
+    for headerno, (header, astmdef) in headers.items():
         if ":" in header:
             topname, _ = header.split(":")
             name = f"ASTM_{topname}"
             if name not in astm:
+                preflabel = en(fixtitle(topname))
                 TopHeader = types.new_class(name, (owlready2.Thing,))
-                TopHeader.prefLabel.append(en(fixtitle(topname)))
+                TopHeader.prefLabel.append(en(preflabel))
+                TopHeader.astmDef.append(en(topheaders[preflabel]))
 
-    for headerno, header in headers.items():
+    for headerno, (header, astmdef) in headers.items():
         if ":" in header:
             topname, preflabel = header.split(":")
             base = astm[f"ASTM_{topname}"]
@@ -139,6 +180,7 @@ with astm:
         Header = types.new_class(f"ASTM_{headerno}", (base,))
         Header.prefLabel.append(en(fixtitle(preflabel)))
         Header.astmNo = headerno
+        Header.astmDef = en(astmdef)
 
 
     # Parse ASTM HTML file
@@ -153,8 +195,6 @@ with astm:
         titles = [t.text for t in term.find_all(
             "div", attrs={"class": "sts-tbx-term"})]
         definition = term.find("div", attrs={"class": "sts-tbx-def"}).text
-        #notes = [note for note in term.find_all(
-        #    "div", attrs={"class": "sts-tbx-note"})]
         notes = [" ".join(list(note.strings)[1:]) for note in term.find_all(
             "div", attrs={"class": "sts-tbx-note"})]
 
@@ -163,6 +203,8 @@ with astm:
 
         if titles[0].endswith("noun"):
             bases = (Header, Noun)
+        elif titles[0].endswith("verb"):
+            bases = (Header, Verb)
         elif titles[0].endswith("adjective"):
             bases = (Header, Adjective)
         elif titles[0].endswith("participle"):
@@ -172,13 +214,24 @@ with astm:
 
         Term = types.new_class(f"ASTM_{termno}", bases)
         Term.prefLabel.append(en(fixtitle(titles[0])))
-        Term.altLabel.extend(en(fixtitle(title)) for title in titles[1:])
+        for title in titles[1:]:
+            if title.lower().startswith("deprecated:"):
+                Term.hiddenLabel.append(en(fixtitle(title[11:])))
+            else:
+                Term.altLabel.append(en(fixtitle(title)))
         Term.astmNo = termno
         Term.astmId = termid
         Term.astmDef = en(fixdef(definition))
         Term.comment.extend(en(fixdef(note)) for note in notes)
 
+# Add additional subclass relations
+for clsname, subclasses in subclassOf.items():
+    astm[clsname].is_a.extend(astm[name] for name in subclasses)
 
+# Convert class docstrings to rdfs:comment
+astm.sync_attributes()
+
+# Add metadata
 astm.metadata.title.append(en(
     'ISO/ASTM 52900:2021 Additive manufacturing - General principles - '
     'Fundamentals and vocabulary'))
@@ -220,4 +273,6 @@ astm.metadata.versionInfo.append(en(version))
 
 astm.set_version(
     version=version, version_iri=f"http://iso.org/{version}/astm#")
+
+# Save to file
 astm.save(thisdir / "astm.ttl", format="turtle", overwrite=True)
